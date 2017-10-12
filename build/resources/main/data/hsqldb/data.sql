cc745956b9f1b4cfccc68edfdbbe00d62b5098ae
bhavankris <bhavanforyou@gmail.com>
2017-10-12 22:56:36 +0530

    updated gitignore

--- build/resources/main/data/hsqldb/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Employee(name,age) values('Bhavan',23);
-INSERT INTO Employee(name,age) values('Kris',24);
-INSERT INTO Employee(name,age) values('Vel',25);
-INSERT INTO Employee(name,age) values('prasanna',40);
